c6c70f933b5c820eaaa7f46a001d094936c8597f
ABE <abe@abewartech.my.id>
2023-03-17 10:36:09 +0700

    ✨ What’s your fleet?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Heading,
+  Button,
+  Grid,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  StackDivider,
+  Stack,
+  Box,
+  ButtonGroup,
+  Container,
+  Checkbox,
+  HStack,
+  VStack
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW="container.lg" mt="24px">
+      <Grid align="center" justify="center" gap={2} autoFlow="row dense">
+        <Card align="center">
+          <CardHeader>
+            <Heading size="md">Penawaran</Heading>
+          </CardHeader>
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              <Box>
+                <Heading mb={5}>What’s your fleet?</Heading>
+                <HStack spacing={4}>
+                  <VStack align="start">
+                    <Checkbox size="lg" colorScheme="green" mb={2} mt={2}>
+                      Air transportation
+                    </Checkbox>
+                    <Checkbox size="lg" colorScheme="green" mb={2}>
+                      Land transportation
+                    </Checkbox>
+                  </VStack>
+                  <VStack align="start">
+                    <Checkbox size="lg" colorScheme="green" mb={2} mt={2}>
+                      Sea transportation
+                    </Checkbox>
+                    <Checkbox size="lg" colorScheme="green" mb={2}>
+                      Inland transportation
+                    </Checkbox>
+                  </VStack>
+                </HStack>
+              </Box>
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue">
+                Submit
+              </Button>
+              <Button variant="ghost" colorScheme="blue">
+                Help?
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      </Grid>
+    </Container>
   );
 }
 
